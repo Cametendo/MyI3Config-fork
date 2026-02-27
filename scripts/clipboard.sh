b2e1a4bd263be822c7c2c0acb364275024a2d05f
Cametendo <cameronmathis08@gmail.com>
2026-02-27 11:03:30 +0100

    clarify clipboard.sh
    clarified clipboard history launch command.

--- scripts/clipboard.sh
+++ scripts/clipboard.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# KISS Clipboard history using clipmenu
 
 # Ensure the daemon is running (it exits if already running)
 clipmenud & 
 
-# Open the menu
-# Use CM_LAUNCHER="rofi" if the repo specifically prefers rofi
+# launches the clipboard history
 CM_LAUNCHER="rofi" clipmenu -dmenu -i -p "Clipboard:"
